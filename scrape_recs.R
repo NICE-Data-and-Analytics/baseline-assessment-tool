208650dfd2466e0672172cf15f6c87f574c17ee9
Vanessa <vkwacko007@gmail.com>
2022-05-04 17:49:54 +0100

    huge nested table

--- scrape_recs.R
+++ scrape_recs.R
@@ -6,17 +6,14 @@ library(rlang)
 rec_url <- "https://www.nice.org.uk/guidance/ng205/chapter/Recommendations"
 rec_html <- read_html(rec_url)
 
-sections <- rec_html %>% 
-    html_elements("div.section h3") %>% 
-    html_text2()
-
-section_id <- rec_html %>% 
-    html_elements("div.chapter > div.section") %>% 
-    html_attr("id") %>% 
-    str_replace_all("\\.", "\\\\.")
-
-n <- section_id[[2]]
-
+section_name_fn <- function(id, html) {
+    css <- sprintf("div#%s h3", id)
+    
+    html %>% 
+        html_elements(css) %>% 
+        html_text2() %>% 
+        str_trim()
+}
 
 subsection_name_fn <- function(id, html) {
     css <- paste0("#", id, " h4.title")
@@ -26,13 +23,17 @@ subsection_name_fn <- function(id, html) {
         html_text2()
 }
 
-subsection_css <- sprintf("div#%s div.section", n)
+subsection_fn <- function(section_id, html) {
+    css <- sprintf("div#%s div.section", section_id)
+    
+    html %>% 
+        html_elements(subsection_css) %>% 
+        html_attr("id") %>% 
+        tibble(id = .) %>% 
+        mutate(title = map_chr(id, subsection_name_fn, rec_html))
+}
 
-subsection_id <- rec_html %>% 
-    html_elements(subsection_css) %>% 
-    html_attr("id") %>% 
-    tibble(id = .) %>% 
-    mutate(title = map_chr(id, subsection_name_fn, rec_html))
+
 
 rec_fn <- function(id, html) {
     css <- sprintf("div#%s div.recommendation_text", id)
@@ -45,52 +46,70 @@ rec_fn <- function(id, html) {
     return(table)
 }
 
-subsection_id <- subsection_id %>% 
-    mutate(recs = map(id, rec_fn, rec_html))
+rec_number_fn <- function(id, html) {
+    rec_number_css <- sprintf("div#%s span.paragraph-number", id)
+    
+    html %>% 
+        html_element(rec_number_css) %>% 
+        html_text2()
+}
 
-rec_id <- "ng205_1_2_1"
-rec_number_css <- sprintf("div#%s span.paragraph-number", rec_id)
+chunk_fn <- function(id, html) {
+    rec_css <- paste0("div#", id)
+    
+    html %>% 
+        html_elements(rec_css) %>% 
+        html_children() %>% 
+        as.character()
+}
 
-rec_number <- rec_html %>% 
-    html_element(rec_number_css) %>% 
-    html_text2()
+format_fn <- function(chunk) {
+    if (str_detect(chunk, '^<p class=\"numbered-paragraph\">')) {
+        chunk %>% 
+            str_remove('^<p class=\"numbered-paragraph\">') %>% 
+            str_remove('(<span class=\"paragraph-number\">)[^>]+(</span>)') %>% 
+            str_remove_all('(<a id=\")[^>]+("></a>)') %>% 
+            str_remove_all('(<a class=\"link\")[^>]+(>)') %>% 
+            str_remove_all('</a>') %>% 
+            str_remove('</p>$') %>% 
+            str_remove_all('\n|\r') %>% 
+            str_trim()
+        
+    } else if (str_detect(chunk, '^<div class=\"itemizedlist indented\">')) {
+        chunk %>% 
+            str_remove_all('(<a id=\")[^>]+("></a>)') %>% 
+            str_remove_all('(<a class=\"link\")[^>]+(>)') %>% 
+            str_remove_all('</a>') %>% 
+            str_extract_all('(?<=<li class=\"listitem\">\r\n)\\s*<p>[^>]+(?=</p>)') %>% 
+            simplify() %>% 
+            str_remove('<p>') %>% 
+            str_trim() %>% 
+            paste("\n\u2022 ", ., collapse = "")
+    }
+}
 
-css <- sprintf("div#%s p", rec_id)
+section_id <- rec_html %>% 
+    html_elements("div.chapter > div.section") %>% 
+    html_attr("id") %>% 
+    str_replace_all("\\.", "\\\\.") %>% 
+    tibble(id = .) %>% 
+    mutate(name = map(id, section_name_fn, rec_html)) %>% 
+    filter(str_starts(name, '\\d')) %>% 
+    mutate(subsection = map(id, subsection_fn, rec_html))
 
-rec <- rec_html %>% 
-    html_elements(css) %>% 
-    html_text2()
+section_id <- section_id %>% 
+    mutate(subsection = map(subsection,
+                            ~ .x %>%
+                                mutate(recs = map(id, rec_fn, rec_html)))) %>% 
+    mutate(subsection = map(subsection,
+                            ~ .x %>%
+                                mutate(recs = map(recs, 
+                                                  ~ .x %>%
+                                                      mutate(number = map_chr(id, rec_number_fn, rec_html),
+                                                             chunk = map(id, chunk_fn, rec_html) %>% 
+                                                                 map(. %>% map(format_fn)))))))
 
-chunk <- rec_html %>% 
-    html_elements("div#ng205_1_2_1") %>% 
-    html_children() %>% 
-    as.character()
-
-chunk[[1]] %>% 
-    str_remove('^<p class=\"numbered-paragraph\">\r\n') %>% 
-    str_remove('(<span class=\"paragraph-number\">)[^>]+(</span>\r\n)') %>% 
-    str_remove_all('(<a id=\")[^>]+("></a>)') %>% 
-    str_remove_all('(<a class=\"link\")[^>]+(>)') %>% 
-    str_remove_all('</a>') %>% 
-    str_remove('</p>$') %>% 
-    str_trim()
-
-test <- chunk[[2]] %>% 
-    str_remove_all('(<a id=\")[^>]+("></a>)') %>% 
-    str_remove_all('(<a class=\"link\")[^>]+(>)') %>% 
-    str_remove_all('</a>') %>% 
-    str_extract_all('(?<=<li class=\"listitem\">\r\n)\\s*<p>[^>]+(?=</p>)') %>% 
-    simplify() %>% 
-    str_remove('<p>') %>% 
-    str_trim() %>% 
-    paste("\u2022 ", .)
-
-    str_extract_all('(?=<li class=\"listitem\">\r\n\\s*<p>)[^>]+(?=</p>)')
 
 html_children(section_recs)
 xml_structure(section_recs)
 # xml2::as_list(section_recs)
-
-rec_id <- section_recs %>% 
-    html_elements("div.recommendation_text") %>% 
-    html_attr("id")
